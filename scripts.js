dda9528987369855cedb53a3316602b4dda15948
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-04-26 17:50:02 +0600

    Added video controls panel

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
-function openFullScreen() {
-    this.classList.add('video_full-screen');
+function openFullScreen(event) {
+    event.currentTarget.classList.add('video_full-screen');
+}
+
+function exitFullScreen(event) {
+    event.stopPropagation();
+
+    const video = event.currentTarget.closest('.video');
+    video.classList.remove('video_full-screen');
 }
 
 (function () {
     const videos = document.querySelectorAll('.video').values();
 
     for (let video of videos) {
-        video.onclick = function() {
-            openFullScreen.bind(this)();
-        }
+        video.addEventListener('click', openFullScreen);
+
+        const exitControl = video.querySelector('.video-controls__exit-fullscreen');
+        exitControl.addEventListener('click', exitFullScreen);
     }
 })();
