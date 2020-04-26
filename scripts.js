469f94f82a4967dc9cf745bc782205b99f15c15c
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-04-26 19:20:38 +0600

    Added controls for brightness and contrast

--- scripts.js
+++ scripts.js
@@ -9,6 +9,22 @@ function exitFullScreen(event) {
     video.classList.remove('video_full-screen');
 }
 
+function changeBrightness(event) {
+    const { value } = event.currentTarget;
+    const videoContainer = event.currentTarget.closest('.video');
+    const video = videoContainer.querySelector('video');
+
+    video.style.filter = `brightness(${value})`;
+}
+
+function changeContrast(event) {
+    const { value } = event.currentTarget;
+    const videoContainer = event.currentTarget.closest('.video');
+    const video = videoContainer.querySelector('video');
+
+    video.style.filter = `contrast(${value})`;
+}
+
 (function () {
     const videos = document.querySelectorAll('.video').values();
 
@@ -17,5 +33,11 @@ function exitFullScreen(event) {
 
         const exitControl = video.querySelector('.video-controls__exit-fullscreen');
         exitControl.addEventListener('click', exitFullScreen);
+
+        const brightnessControl = video.querySelector('.video-controls__brightness input[type=range]');
+        brightnessControl.addEventListener('input', changeBrightness);
+
+        const contrastControl = video.querySelector('.video-controls__contrast input[type=range]');
+        contrastControl.addEventListener('input', changeContrast);
     }
 })();
